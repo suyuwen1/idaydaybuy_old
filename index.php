38fc66211b1d3ab21e5f438dd6dee5233d5fea07
suyuwen1 <suyuwen1@126.com>
2015-01-02 00:51:09 +0800

    2015.1.2

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
-<?php
-/**
- * 
- * @authors Your Name (you@example.org)
- * @date    2015-01-01 16:55:41
- * @version $Id$
- */
-
-class ClassName extends AnotherClass {
-    
-    function __construct(){
-        
-    }
-}
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>天天买-我们只是产品的搬用工</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<link rel="stylesheet" type="text/css" href="css/font-awesome-4.2.0/css/font-awesome.min.css">
+<link href="css/i.css" type="text/css" rel="stylesheet">
+<script src="js/jquery-1.11.2.min.js" type="text/javascript"></script>
+<script src="js/i.js" type="text/javascript"></script>
+</head>
+<body>
+    <div id="b">
+    	<div id="m">
+    		<div id="t"><span id="t-bars">天天买&nbsp;<i class="fa fa-bars fa-fw"></i></span></div>
+    		<div id="c" style="font-size:100px"><input type="text"></div>
+    		<div id="d" style="font-size:100px">123s</div>
+    	</div>
+    	<div id="bg"><img id="bg-img" src="img/2.jpg"></div>
+    </div>
+</body>
+</html>
---
+++ js/i.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2015-01-01 17:19:09
+ * @version $Id$
+ */
+
