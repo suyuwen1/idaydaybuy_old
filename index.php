91e64f6139087c11882a8834ef78da74d18e6fbc
suyuwen1 <suyuwen1@126.com>
2015-01-01 17:01:44 +0800

    2015.1.1

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2015-01-01 16:55:41
+ * @version $Id$
+ */
+
+class ClassName extends AnotherClass {
+    
+    function __construct(){
+        
+    }
+}
