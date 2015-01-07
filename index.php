16d00fc7e32cf786ef260522a9176666ab3c8df3
suyuwen1 <suyuwen1@126.com>
2015-01-07 17:25:20 +0800

    2015.1.7

--- index.php
+++ index.php
@@ -16,16 +16,22 @@
     	<div id="t">
 			<div id="t-bars">
 				<ul>
-					<li class="t-bars-li tlogo"><a class="t-bars-a" href="#">天天买</a></li>
-					<li class="t-bars-li"><a class="t-bars-a" href="#">今日今日</a></li>
-					<li class="t-bars-li"><a class="t-bars-a" href="#">今日今日</a></li>
+					<li class="t-bars-li tlogo"><a class="t-bars-a" href="#">爱国者</a></li>
+					<li class="t-bars-li"><a class="t-bars-a" href="#">爱国者</a></li>
+					<li class="t-bars-li"><a class="t-bars-a" href="#">爱国者</a></li>
+					<li class="tsou"><form><input type="text"><button id="t-submit" style="submit"><i class="fa fa-search"></i></button></form></li>
 				</ul>
 			</div>
     	</div>
     	<div id="c">
-			<div class="c-lists"></div>
+			<ul>
+				<li class="c-li"><div class="c-li-img"><img src="img/123.png"></div><div class="c-li-info">1</div></li>
+				<li class="c-li"><div class="c-li-img"><img src="img/456.jpg"></div><div class="c-li-info">1</div></li>
+				<li class="c-li"><div class="c-li-img"><img src="img/123.png"></div><div class="c-li-info">1</div></li>
+			</ul>
     	</div>
-    	<div id="d">123s</div>
+    	<span id="top"><i class="fa fa-arrow-up"></i></span>
+    	<div id="d">京ICP备12017597号-1</div>
     	</div>
     </div>
 </body>
