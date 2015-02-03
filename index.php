bce86547bda65c5ff041f089bb420412ada568b8
suyuwen1 <suyuwen1@126.com>
2015-02-03 21:24:38 +0800

    2015.2.3

--- index.php
+++ index.php
@@ -19,8 +19,9 @@ $page='index';
 							break;
 						}
 						// echo $k;
+						$vv=mb_substr($v['description'], 0, 200, 'utf-8');
 						echo <<< END
-						<li class="c-li"><div class="c-li-img"><img src="{$v['img']}"></div><div class="c-li-info"><div class="title"><a href="{$v['id']}.html">{$v['title']}</a></div><div class="content">{$v['description']}</div><div class="qita"><div class="price_l"><div class="rmb">&yen;</div><div class="price">{$v['price']}</div><div class="store">{$v['store']}</div></div><div class="links"><a target="_blank" href="{$v['links']}">购买</a></div></div></div></li>
+						<li class="c-li"><div class="c-li-img"><img src="{$v['img']}"></div><div class="c-li-info"><div class="title"><a href="{$v['id']}.html">{$v['title']}</a></div><div class="content">{$vv}</div><div class="qita"><div class="price_l"><div class="rmb">&yen;</div><div class="price">{$v['price']}</div><div class="store">{$v['store']}</div></div><div class="links"><a target="_blank" href="{$v['links']}">购买</a></div></div></div></li>
 END;
 					}
 					echo '<div id="fanye">';
--- read.php
+++ read.php
@@ -16,6 +16,7 @@ $s=$M->biao('products')->where('id='.$_GET['id'])->limit(0,1)->select();
 if ($s) {
 	$title=$s[0]['title'];
 	$description=mb_substr($s[0]['description'], 0, 200, 'utf-8');
+	$s=$s[0];
 }else{
 	header('HTTP/1.1 404 Not Found');
 	header("status: 404 Not Found");
@@ -28,6 +29,12 @@ if ($s) {
 <?php include('header.php');?>
 <div id="read">
 	<div id="read-title"><?php echo $title;?></div>
-	<div id="read-content"><?php echo stripcslashes($s[0]['content']);?></div>
+	<div id="read-content">
+		<div id="read-content-info">
+			<div class="c-li-img"><img src="<?php echo $s['img'];?>"></div>
+			<div class="c-li-info"><div class="content content2"><?php echo $description;?></div><div class="store store2"><?php echo $s['store'];?></div><div class="qita"><div class="price_l"><div class="rmb">&yen;</div><div class="price"><?php echo $s['price'];?></div></div><div class="links"><a target="_blank" href="<?php echo $s['links'];?>">购买</a></div></div></div>
+		</div>
+		<div><?php echo stripcslashes($s[0]['content']);?></div>
+	</div>
 </div>
 <?php include('footer.php');?>
