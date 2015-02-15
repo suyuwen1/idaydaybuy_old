e055fb874709f3e67ac0c0d56cc010c597ede8f3
suyuwen1 <suyuwen1@126.com>
2015-02-15 17:31:42 +0800

    2015.2.15

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="renderer" content="webkit">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport"content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 	<title><?php echo empty($title)?'天天买-我们只是产品的搬用工！':$title.'-天天买';?></title>
 	<meta name="description" content="<?php echo empty($description)?'天天买-我们只是产品的搬用工！':$description;?>">
 	<meta name="keywords" content="天天买,天天特卖,促销活动">
@@ -17,11 +18,8 @@
 	<div id="b">
 		<div id="t">
 			<div id="t-bars">
-				<ul>
-					<li class="t-bars-li tlogo"><a class="t-bars-a" href="index.php">天天买</a></li>
-					<li class="t-bars-li"><a class="t-bars-a" href="#">天天买特卖</a></li>
-					<li class="t-bars-li"><a class="t-bars-a" href="#"></a></li>
-					<li class="tsou"><form action="search.php" method="get"><input id="sou-text" placeholder="搜索" name="s" type="text" value="<?php echo (empty($_POST['sou-text']))? '' : $_POST['sou-text'];?>"><button id="t-submit" style="submit"><i class="fa fa-search"></i></button></form></li>
-				</ul>
+					<div class="t-bars-li tlogo"><a class="t-bars-a" href="index.php">天天买</a></div>
+					<div class="t-bars-li"><a class="t-bars-a" href="#">天天买特卖</a></div>
+					<div class="tsou"><form action="search.php" method="get"><input id="sou-text" placeholder="搜索" name="s" type="text" value="<?php echo (empty($_POST['sou-text']))? '' : $_POST['sou-text'];?>"><button id="t-submit" style="submit"><i class="fa fa-search"></i></button></form></div>
 			</div>
 		</div>
